4a743f2c7177748370caa66f1bab7393e8bac716
Alex Francois and Lina Shadrach <lina@>
2017-01-04 15:05:02 -0800

    add count function for stolen bikes

--- build/js/app.js
+++ build/js/app.js
@@ -24,27 +24,10 @@ $(document).ready(function(){
     var location = $('#location').val();
     var make = $("#manufacturer").val();
     $("#make").val("");
-    console.log("make" + make);
-    var bike = new Bike();
-    var foundCount = 0;
-    var responseFound = true;
-    var pageCount = 0;
-    do{
-      pageCount++;
-      $.get('https://bikeindex.org:443/api/v3/search?page=' + pageCount +'&per_page=100&manufacturer=' + make + '&location=' + location + '&stolenness=stolen&access_token=' + apiKey).then(function(response){
-        if (response.bikes.length>0) {
-          responseFound = true;
-          foundCount += bike.countManufacturer(response.bikes);
-          console.log(foundCount);
-        } else {
-          responseFound = false;
-        }
-        });
-    } while (responseFound===true);
-
-      $('#result').text(foundCount + " bikes were found matching that description.");
-
-
+    $.get('https://bikeindex.org:443/api/v3/search/count?manufacturer=' + make + '&location=' + location + ' &stolenness=stolen&access_token=' + apiKey).then(function(response){
+      console.log(response);
+      $('#result').text(response.proximity + " bikes were found matching that description.");
+    });
   });
 });
 
